c4d5a03cd928e9b26b2cea73c7bbc86f789d788f
JB <shin0343@mail.hongik.ac.kr>
2020-11-07 22:57:40 +0900

    Solving LC 38 ..

---
+++ LeetCode/LC38.cpp
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/count-and-say/
+
+#include <string>
+using namespace std;
+
+int nMemo[31];
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        
+    }
+};
