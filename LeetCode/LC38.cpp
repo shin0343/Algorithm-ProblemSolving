87225dcab35c34e1b3e11b3fe14fff5c2aeb31d5
JB <shin0343@mail.hongik.ac.kr>
2020-11-08 12:06:03 +0900

    Solved LC 38

--- LeetCode/LC38.cpp
+++ LeetCode/LC38.cpp
@@ -1,13 +1,46 @@
 //https://leetcode.com/problems/count-and-say/
 
 #include <string>
+#include <iostream>
 using namespace std;
 
-int nMemo[31];
+string nMemo[31];
+string Ans;
 
 class Solution {
 public:
-    string countAndSay(int n) {
-        
-    }
+    void dfs(string cas, int cnt, int n)
+    {
+        if(cnt>=n)
+        {
+            Ans=cas;
+            return;
+        }
+
+        string tmp="";
+        int numCnt=0;
+
+        for(int i=0;i<cas.length();i++)
+        {
+            if(i>0 && cas[i]!=cas[i-1])
+            {
+                tmp+=to_string(numCnt);
+                tmp+=cas[i-1];
+                numCnt=0;
+            }
+            
+            numCnt+=1;
+        }
+
+        tmp+=to_string(numCnt);
+        tmp+=cas[cas.length()-1];
+
+        dfs(tmp,cnt+1,n);
+    };
+
+    string countAndSay(int n)
+    {
+        dfs("1",1,n);
+        return Ans;
+    };
 };
