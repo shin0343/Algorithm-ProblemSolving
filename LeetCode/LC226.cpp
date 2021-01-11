bb6276649ef0bf6c55bd66b833683d3bdc0babc2
JB <shin0343@mail.hongik.ac.kr>
2021-01-11 20:43:07 +0900

    Solved LC 226

--- LeetCode/LC226.cpp
+++ LeetCode/LC226.cpp
@@ -14,5 +14,16 @@ class Solution
 public:
     TreeNode *invertTree(TreeNode *root)
     {
+        if (root == nullptr)
+        {
+            return nullptr;
+        }
+
+        TreeNode *right = invertTree(root->right);
+        TreeNode *left = invertTree(root->left);
+        root->left = right;
+        root->right = left;
+
+        return root;
     }
 };
