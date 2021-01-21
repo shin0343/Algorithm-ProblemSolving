7f46245f653001ea18bf6accd6db6a3f6340d761
JB <shin0343@mail.hongik.ac.kr>
2021-01-21 20:08:51 +0900

    Solved LC 283+@

--- LeetCode/LC283.cpp
+++ LeetCode/LC283.cpp
@@ -1,3 +1,77 @@
+// class Solution
+// {
+// public:
+//     void moveZeroes(vector<int> &nums)
+//     {
+//         vector<int> ans;
+//         int zeroCnt = 0;
+//         for (int i = 0; i < nums.size(); i++)
+//         {
+//             if (nums[i] == 0)
+//             {
+//                 zeroCnt += 1;
+//             }
+//         }
+
+//         for (int i = 0; i < nums.size(); i++)
+//         {
+//             if (nums[i] == 0)
+//             {
+//                 continue;
+//             }
+
+//             ans.push_back(nums[i]);
+//         }
+
+//         for (int i = 0; i < zeroCnt; i++)
+//         {
+//             ans.push_back(0);
+//         }
+
+//         for (int i = 0; i < nums.size(); i++)
+//         {
+//             nums[i] = ans[i];
+//         }
+//     }
+// };
+
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        vector<int> ans;
+        int zeroCnt = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 0)
+            {
+                zeroCnt += 1;
+            }
+        }
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 0)
+            {
+                continue;
+            }
+
+            ans.push_back(nums[i]);
+        }
+
+        for (int i = 0; i < zeroCnt; i++)
+        {
+            ans.push_back(0);
+        }
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            nums[i] = ans[i];
+        }
+    }
+};
+
 class Solution
 {
 public:
