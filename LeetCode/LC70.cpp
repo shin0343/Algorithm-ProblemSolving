76d05b9e956351010938aca9c3a5e305616ac76d
JB <shin0343@mail.hongik.ac.kr>
2021-02-28 17:35:43 +0900

    Solved LC 70

---
+++ LeetCode/LC70.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int dp[46];
+    int climbStairs(int n)
+    {
+        memset(dp,0,sizeof(dp));
+        
+        dp[1]=1;
+        dp[2]=2;
+
+        for(int i=3;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+    }
+};
